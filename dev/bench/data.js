d7be4bf34199b414bb10ccec0c642d2ef1dabc42
github-action-benchmark <github@users.noreply.github.com>
2023-10-17 07:11:41 +0000

    add Milvus backup restore performance test (pytest) benchmark result for 98cded75464d899ad36b3a2212c2188a9fc0c5bb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697523643040,
+  "lastUpdate": 1697526701961,
   "repoUrl": "https://github.com/zhuwenxing/milvus-backup",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -110,6 +110,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.010806434070077088",
             "extra": "mean: 30.05640818940001 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wxzhuyeah@gmail.com",
+            "name": "zhuwenxing",
+            "username": "zhuwenxing"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "98cded75464d899ad36b3a2212c2188a9fc0c5bb",
+          "message": "Merge pull request #10 from zhuwenxing/test\n\nupdate",
+          "timestamp": "2023-10-17T02:04:33-05:00",
+          "tree_id": "f9b873b8b02d65e12623d7577dd4a93e0ff2356a",
+          "url": "https://github.com/zhuwenxing/milvus-backup/commit/98cded75464d899ad36b3a2212c2188a9fc0c5bb"
+        },
+        "date": 1697526700903,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "testcases/test_perf.py::TestPerf::test_milvus_create_backup_perf[100000]",
+            "value": 1.0217464728604122,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0851503368502414",
+            "extra": "mean: 978.7163710000073 msec\nrounds: 5"
+          },
+          {
+            "name": "testcases/test_perf.py::TestPerf::test_milvus_restore_backup_perf[100000]",
+            "value": 0.033235785645724006,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0033175489331248383",
+            "extra": "mean: 30.088050592800005 sec\nrounds: 5"
           }
         ]
       }
