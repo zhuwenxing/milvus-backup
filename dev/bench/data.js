cf1c14fdc1820c755c4b94656a84e0dfb09203d0
github-action-benchmark <github@users.noreply.github.com>
2024-01-03 04:26:45 +0000

    add Milvus backup restore performance test (pytest) benchmark result for 98cded75464d899ad36b3a2212c2188a9fc0c5bb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704169606588,
+  "lastUpdate": 1704256005537,
   "repoUrl": "https://github.com/zhuwenxing/milvus-backup",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -3056,6 +3056,42 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 2.742380869897021",
             "extra": "mean: 27.051240089599993 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "zhuwenxing",
+            "username": "zhuwenxing",
+            "email": "wxzhuyeah@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "98cded75464d899ad36b3a2212c2188a9fc0c5bb",
+          "message": "Merge pull request #10 from zhuwenxing/test\n\nupdate",
+          "timestamp": "2023-10-17T07:04:33Z",
+          "url": "https://github.com/zhuwenxing/milvus-backup/commit/98cded75464d899ad36b3a2212c2188a9fc0c5bb"
+        },
+        "date": 1704256004560,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "testcases/test_perf.py::TestPerf::test_milvus_create_backup_perf[100000]",
+            "value": 2.3882393951911176,
+            "unit": "iter/sec",
+            "range": "stddev: 0.006821200954169329",
+            "extra": "mean: 418.7184927999965 msec\nrounds: 5"
+          },
+          {
+            "name": "testcases/test_perf.py::TestPerf::test_milvus_restore_backup_perf[100000]",
+            "value": 0.03992270356901613,
+            "unit": "iter/sec",
+            "range": "stddev: 0.007533638884541651",
+            "extra": "mean: 25.048403805400007 sec\nrounds: 5"
           }
         ]
       }
